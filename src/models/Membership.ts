934bdf7373035c8b317422f72a18ed62e4f0bd41
mause24 <omararenasuni@gmail.com>
2026-06-11 16:58:54 -0500

    feat(model): update Membership Sequelize model

--- src/models/Membership.ts
+++ src/models/Membership.ts
@@ -1,38 +1,41 @@
 import {
-  CreationOptional,
-  DataTypes,
-  InferAttributes,
-  InferCreationAttributes,
-  Model,
-} from "sequelize";
-import { sequelize } from "../database";
+    CreationOptional,
+    DataTypes,
+    InferAttributes,
+    InferCreationAttributes,
+    Model,
+} from "sequelize"
+import { sequelize } from "../database"
 
-class Membership extends Model<InferAttributes<Membership>, InferCreationAttributes<Membership>> {
-  declare id: CreationOptional<number>;
-  declare name: string;
-  declare price: string;
+class Membership extends Model<
+    InferAttributes<Membership>,
+    InferCreationAttributes<Membership>
+> {
+    declare id: CreationOptional<number>
+    declare name: string
+    declare price: string
 }
 
 Membership.init(
-  {
-    id: {
-      type: DataTypes.BIGINT,
-      autoIncrement: true,
-      primaryKey: true,
+    {
+        id: {
+            type: DataTypes.BIGINT,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        name: {
+            type: DataTypes.STRING(100),
+            allowNull: false,
+        },
+        price: {
+            type: DataTypes.DECIMAL(10, 2),
+            allowNull: false,
+        },
     },
-    name: {
-      type: DataTypes.STRING(100),
-      allowNull: false,
-    },
-    price: {
-      type: DataTypes.DECIMAL(10, 2),
-      allowNull: false,
-    },
-  },
-  {
-    sequelize,
-    paranoid: true,
-  }
-);
+    {
+        sequelize,
+        paranoid: true,
+    }
+)
 
-export default Membership;
+export default Membership
