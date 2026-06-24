9c78e45411327cae0e7e0c2c80db3fa5b6e7f060
mause24 <omararenasuni@gmail.com>
2026-06-24 13:56:31 -0500

    fix: update the subscription post route to disable that only the admin user can create it

--- src/routes/subscriptionRoutes.ts
+++ src/routes/subscriptionRoutes.ts
@@ -29,7 +29,7 @@ subscriptionRoutes.get("/", authentication(), getAllSubscriptions)
 // CREATE – POST /subscription (solo ADMIN)
 subscriptionRoutes.post(
     "/",
-    authentication([PROFILES.ADMIN]),
+    authentication(),
     validatorBody(createSubscriptionSchema),
     createSubscription
 )
